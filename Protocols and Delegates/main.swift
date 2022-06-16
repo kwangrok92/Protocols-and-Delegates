3c6f7686d8375173ef09367420eaaa59d4c7648b
Cdeer <kwangrok92@naver.com>
2022-06-16 16:14:40 +0900

    finished

--- Protocols and Delegates/main.swift
+++ Protocols and Delegates/main.swift
@@ -5,4 +5,57 @@ protocol AdvancedLifeSupport {
 
 class EmergencyCallHandler {
     var delgate: AdvancedLifeSupport?
+    
+    func assessSituation() {
+        print("Can you tell me what happend?")
+    }
+    
+    func medicalEmergency() {
+        delgate?.performCPR()
+    }
 }
+
+struct Paramedic: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delgate = self
+    }
+    
+    func performCPR() {
+        print("The paramedic does chest compressions, 30 per second.")
+    }
+}
+
+class Doctor: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delgate = self
+    }
+    
+    func performCPR() {
+        print("The doctor does chest compressions, 30 per second.")
+    }
+    
+    func useStethescope() {
+        print("Listening for heart sound.")
+    }
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Sings staying alive by the Beegees.")
+    }
+    
+    func useElectricDrill() {
+        print("Whirr...")
+    }
+}
+
+
+
+let emilio = EmergencyCallHandler()
+let angela = Surgeon(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
